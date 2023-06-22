57c6c7aec182782a48e24cd0d8950fcbd207d4e7
MatBos97 <86076672+MatBos97@users.noreply.github.com>
2023-06-22 18:25:04 +0200

    Set up project and mvn clean install runs without error

---
+++ src/test/java/mathijs/bos/garage_app/GarageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mathijs.bos.garage_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
