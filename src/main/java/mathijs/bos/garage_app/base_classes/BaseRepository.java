28fd38e482edae822999d1243cc6823664155f84
MatBos97 <86076672+MatBos97@users.noreply.github.com>
2023-06-22 21:25:24 +0200

    Create the base classes for the controller, service and repository

---
+++ src/main/java/mathijs/bos/garage_app/base_classes/BaseRepository.java
@@ -0,0 +1,7 @@
+package mathijs.bos.garage_app.base_classes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T> extends JpaRepository<T, Long> {
+}
