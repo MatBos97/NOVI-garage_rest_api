c3659bcffaadd26c5dcae264332030fd24466e26
MatBos97 <86076672+MatBos97@users.noreply.github.com>
2023-06-26 23:45:57 +0200

    Added function to upload car papers

--- src/main/java/mathijs/bos/garage_app/car/CarController.java
+++ src/main/java/mathijs/bos/garage_app/car/CarController.java
@@ -3,15 +3,32 @@ package mathijs.bos.garage_app.car;
 import mathijs.bos.garage_app.base_classes.BaseController;
 import mathijs.bos.garage_app.base_classes.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/car")
 public class CarController extends BaseController<Car> {
 
+    private final CarService carService;
+
     @Autowired
-    public CarController(CarService service) {
-        super(service);
+    public CarController(CarService carService) {
+        super(carService);
+        this.carService = carService;
+    }
+
+    public ResponseEntity<String> uploadCarPapers(@PathVariable Long carId, @RequestParam("file")MultipartFile file){
+        try{
+            carService.saveCarPapers(carId, file);
+            return ResponseEntity.ok("Car papers uplaoded successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload car papers.");
+        }
     }
 }
