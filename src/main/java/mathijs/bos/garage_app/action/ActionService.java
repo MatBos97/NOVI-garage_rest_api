441de5b0b3d9966017499574447ba45566aeeddf
MatBos97 <86076672+MatBos97@users.noreply.github.com>
2023-07-10 19:25:34 +0200

    Created the first update method.

--- src/main/java/mathijs/bos/garage_app/action/ActionService.java
+++ src/main/java/mathijs/bos/garage_app/action/ActionService.java
@@ -1,12 +1,33 @@
 package mathijs.bos.garage_app.action;
 
+import jakarta.persistence.EntityNotFoundException;
 import mathijs.bos.garage_app.base_classes.BaseRepository;
 import mathijs.bos.garage_app.base_classes.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ActionService extends BaseService<Action> {
-    public ActionService(ActionRepository repository) {
-        super(repository);
+
+    private ActionRepository actionRepository;
+
+    @Autowired
+    public ActionService(ActionRepository actionRepository) {
+        super(actionRepository);
+        this.actionRepository = actionRepository;
+    }
+
+    public Optional<Action> update(Long id, Action newAction) {
+        return actionRepository.findById(id)
+                .map(action -> {
+                    action.setId(newAction.getId());
+                    action.setName(newAction.getName());
+                    action.setPrice(newAction.getPrice());
+
+                    actionRepository.save(action);
+                    return action;
+                });
     }
 }
