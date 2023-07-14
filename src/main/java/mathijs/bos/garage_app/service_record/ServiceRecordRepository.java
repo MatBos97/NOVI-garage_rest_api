c6032309b3624010acff236f27e73cc37704a604
MatBos97 <86076672+MatBos97@users.noreply.github.com>
2023-07-14 15:44:19 +0200

    Laid down the ground works for retrieving customers whose cars are ready for pickup.

--- src/main/java/mathijs/bos/garage_app/service_record/ServiceRecordRepository.java
+++ src/main/java/mathijs/bos/garage_app/service_record/ServiceRecordRepository.java
@@ -2,5 +2,8 @@ package mathijs.bos.garage_app.service_record;
 
 import mathijs.bos.garage_app.base_classes.BaseRepository;
 
+import java.util.List;
+
 public interface ServiceRecordRepository extends BaseRepository<ServiceRecord, Long> {
+    List<ServiceRecord> findByStatus(Status status);
 }
