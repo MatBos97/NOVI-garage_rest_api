175a83f76fa34fc4d0cc44a914fa9d0581257123
MatBos97 <86076672+MatBos97@users.noreply.github.com>
2023-07-22 19:23:42 +0200

    Finished the first update and create method using DTO's and mappers

--- src/main/java/mathijs/bos/garage_app/car/CarService.java
+++ src/main/java/mathijs/bos/garage_app/car/CarService.java
@@ -2,29 +2,80 @@ package mathijs.bos.garage_app.car;
 
 import jakarta.persistence.EntityNotFoundException;
 import mathijs.bos.garage_app.base_classes.BaseService;
+import mathijs.bos.garage_app.car_papers.CarPapers;
+import mathijs.bos.garage_app.car_papers.CarPapersRepository;
+import mathijs.bos.garage_app.customer.Customer;
+import mathijs.bos.garage_app.customer.CustomerRepository;
+import mathijs.bos.garage_app.service_record.ServiceRecord;
+import mathijs.bos.garage_app.service_record.ServiceRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarService extends BaseService<Car, CarDTO, Long> {
 
-    private final CarRepository repository;
-    private final CarMapper mapper;
+    private final CarRepository carRepository;
+    private final CustomerRepository customerRepository;
+    private final ServiceRecordRepository serviceRecordRepository;
+    private final CarMapper carMapper;
+    private final CarPapersRepository carPapersRepository;
 
     @Autowired
-    public CarService(CarRepository repository, CarMapper mapper) {
-        super(repository, mapper);
-        this.repository = repository;
-        this.mapper = mapper;
+    public CarService(
+            CarRepository carRepository,
+            CarMapper carMapper,
+            CustomerRepository customerRepository,
+            ServiceRecordRepository serviceRecordRepository,
+            CarPapersRepository carPapersRepository){
+        super(carRepository, carMapper);
+        this.carRepository = carRepository;
+        this.customerRepository = customerRepository;
+        this.serviceRecordRepository = serviceRecordRepository;
+        this.carMapper = carMapper;
+        this.carPapersRepository = carPapersRepository;
     }
 
     @Override
     public Car create(CarDTO dto) throws EntityNotFoundException {
-        return null;
+        Car car = carMapper.toEntity(dto);
+
+        Customer customer = customerRepository.findById(dto.getCustomerId()).orElseThrow(EntityNotFoundException::new);
+
+        List<ServiceRecord> serviceRecords = dto.getServiceRecordIdList().stream()
+                .map(
+                        serviceRecordId -> serviceRecordRepository.findById(serviceRecordId).orElseThrow(EntityNotFoundException::new)
+                ).toList();
+
+        List<CarPapers> carPapers = dto.getCarPapersIdList().stream()
+                .map(
+                        carPapersId -> carPapersRepository.findById(carPapersId).orElseThrow(EntityNotFoundException::new)
+                ).toList();
+
+        car.setCustomer(customer);
+        car.setServiceRecords(serviceRecords);
+
+        return car;
     }
 
     @Override
-    public Car update(Long aLong, CarDTO dto) throws EntityNotFoundException {
-        return null;
+    public Car update(Long id, CarDTO dto) throws EntityNotFoundException {
+        return carRepository.findById(id)
+                .map(car -> {
+                    car.setId(dto.getId());
+                    car.setCustomer(customerRepository.findById(dto.getCustomerId()).orElseThrow(EntityNotFoundException::new));
+                    car.setServiceRecords(dto.getServiceRecordIdList().stream()
+                            .map(
+                                    serviceRecordId -> serviceRecordRepository.findById(serviceRecordId).orElseThrow(EntityNotFoundException::new))
+                            .toList());
+                    car.setCarPapers(dto.getCarPapersIdList().stream()
+                            .map(
+                                    carPapersId -> carPapersRepository.findById(carPapersId).orElseThrow(EntityNotFoundException::new)
+                            ).toList());
+
+                    return carRepository.save(car);
+                }).orElseThrow(EntityNotFoundException::new);
     }
 }
